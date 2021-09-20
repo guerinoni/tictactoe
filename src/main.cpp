f02d5cd3f322bc1cd56763e561506b719949ff65
Matteo Nistri <mnistri@develer.com>
2021-09-20 19:30:38 +0200

    Clang-format, update screenshot

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include "Game.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -13,17 +13,19 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
 
     qmlRegisterUncreatableType<Game>("Game.enums", 1, 0, "GameFinishedEnum",
-                                            "Not creatable as it is an enum type.");
+        "Not creatable as it is an enum type.");
 
     Game game;
     engine.rootContext()->setContextProperty("game", &game);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject* obj, const QUrl& objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
