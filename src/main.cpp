6070d690c5392829e219e97be855ffbc73d3b1bf
Matteo Nistri <mnistri@develer.com>
2021-09-20 17:49:35 +0200

    Update CMakeLists for qml, remove MainWindow, add first qml interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
-#include "MainWindow.hpp"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 
-#include <QApplication>
+#include "Game.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
